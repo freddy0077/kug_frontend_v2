5f345af9a073e001613a5900499776bc2e352aaf
freddy-betswap <150654880+freddy-betswap@users.noreply.github.com>
2025-03-25 15:58:07 +0000

    Restrict /dogs/new page to ADMIN users only

--- src/app/dogs/new/page.tsx
+++ src/app/dogs/new/page.tsx
@@ -16,12 +16,12 @@ export default function AddNewDogPage() {
         {/* Back button */}
         <div className="mb-6">
           <Link 
-            href="/dogs"
+            href="/dogs" 
             className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
           >
             <svg className="-ml-1 mr-2 h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
-            </svg>
+            </svg>  
             Back to Dogs
           </Link>
         </div>
